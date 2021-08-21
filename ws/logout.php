d69e2c5c896467f2d0245d67950637fb8e3d8170
vamsi mathala <matalav55@gmail.com>
2021-08-21 13:07:17 +0530

    .

--- ws/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-include('config.php');
-include('dblayer.php');
-    session_start();
-    session_destroy();
-    $jsonresponse = array('code' => '200', 'status' => 'Logged Out Sucessfully');
-    echo json_encode($jsonresponse);
-?>
